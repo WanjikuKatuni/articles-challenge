b70505de285ba03c72770a59ea64a52645593d01
wanjiku <annritawanjiku77@gmail.com>
2022-10-31 14:45:27 +0300

    make the all authors magaiznes unique

--- app/models/author.rb
+++ app/models/author.rb
@@ -22,7 +22,7 @@ class Author
   # return a unique array of magazine instances for which the author has contributed to
   def magazines
     all_authors_magazines = articles.map{|article| article.magazine}
-    
+    all_authors_magazines.uniq
   end
 
 
