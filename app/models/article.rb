5ad4ab80041a49b0dabf159d5a02cf05199996a3
wanjiku <annritawanjiku77@gmail.com>
2022-10-31 14:28:27 +0300

    initialise author, mag and titile,& returns an array of article instances

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,20 @@
 class Article
 
+    attr_reader :author, :magazine, :title
+
+    # array of all article instances
+
+    @@all = []
+
+    def initialize(author, magazine, title)
+
+        @author = author
+        @magazine = magazine
+        @title = title
+
+        # sends all instances to array
+        @@all << self
+    end
+
 
 end
