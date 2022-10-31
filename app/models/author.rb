87718ed7ec72a49492855634cdbeecc07420dea3
wanjiku <annritawanjiku77@gmail.com>
2022-10-31 14:44:59 +0300

    return array of magaine instances of the aithor

--- app/models/author.rb
+++ app/models/author.rb
@@ -19,6 +19,13 @@ class Author
     Article.all.select{|article| article.author == self.name}
   end
 
+  # return a unique array of magazine instances for which the author has contributed to
+  def magazines
+    all_authors_magazines = articles.map{|article| article.magazine}
+    
+  end
+
+
 
 
   
