9ebb5202b1c4d26ed8f28716168c9e55b209f1d8
wanjiku <annritawanjiku77@gmail.com>
2022-10-31 14:19:38 +0300

    returns an array of all instances

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -2,9 +2,14 @@ class Magazine
   attr_accessor :name, :category
 
 
+  @@all = []
+
   def initialize(name, category)
     @name = name
     @category = category
+
+    # return array of magazine instances
+    @@all << self
 
   end
 
