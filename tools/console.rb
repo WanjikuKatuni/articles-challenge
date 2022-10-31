1020722f3c33d45e98c970d7cbd1acc648c2463d
wanjiku <annritawanjiku77@gmail.com>
2022-10-31 15:49:45 +0300

    test

--- tools/console.rb
+++ tools/console.rb
@@ -7,7 +7,20 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+# americanah = Author.new("Chimamanda Ngozi")
+# half_of_a_yellow_sun = Author.new("Chimamanda Ngozi")
+sex_lives_of_african_women = Author.new("Nana Darkoa")
 
+readers_digest = Magazine.new("readers digest", "african_stories")
+all_african = Magazine.new("all_African", "history")
+african_women = Magazine.new("african women", "women stories")
+
+article_1 = Article.new(article1, readers_digest, "homegoing")
+article_2 = Article.new(article2, readers_digest, "wahala")
+
+
+sex_lives_of_african_women.add_article(readers_digest, "half a yellow sun")
+sex_lives_of_african_women.add_article(readers_digest, "americannah")
 
 
 
