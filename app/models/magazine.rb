b131f95c59a0df9a71d024cdb309329a8e9a8043
wanjiku <annritawanjiku77@gmail.com>
2022-10-31 14:56:26 +0300

    returns an array of author isntances who have written for the magazine

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -17,5 +17,15 @@ class Magazine
     @@all << self
   end
 
+  # return an array of author isntances who have written for the magazine.
+  def get_articles
+    Article.all.select{|article| article.magazine.name == self.name}
+  end
+
+  def contributors
+    get_articles.collect{|article| article.author}.uniq
+    
+  end
+
 
 end
